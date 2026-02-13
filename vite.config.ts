f5c0c873217fa0a2e7035824f559b34e05c9f23a
Jake Espinosa <jacob.sa.espinosa@gmail.com>
2026-02-13 16:34:43 -0700

    remove ts and setup vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   server: {
-    open: "./src/index.html"
+    open: "./src/index.html",
   },
+  plugins: [
+    tailwindcss(),
+  ],
+  root: "./src",
+  build: {
+    outDir: "../dist",
+    emptyOutDir: true,
+  }
 });
